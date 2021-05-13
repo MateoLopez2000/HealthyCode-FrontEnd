2c83068fd35bdb64e3aa823238f8ec19ca60e013
MateoLopez2000 <lm67427572@gmail.com>
2021-05-12 20:01:01 -0400

    fixed some issues from Sossi

--- src/views/ResultPage.js
+++ src/views/ResultPage.js
@@ -1,39 +1,37 @@
-
 import './ResultPage.css';
-import SearchBar from "./components/SearchBar1/SearchBarFilter"
-import Header from "./components/Header/Header"
-import Footer from "./components/Footer/Footer"
-import FooterLinks from "./components/FooterLinks/FooterLinks"
-import MovieLabel from "./components/MovieLabel/MovieLabel"
+import SearchBar from '../components/SearchBar1/SearchBarFilter';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import FooterLinks from '../components/FooterLinks/FooterLinks';
+import MovieLabel from '../components/MovieLabel/MovieLabel';
 
 function ResultPage() {
-  const { fromSearchPage } = this.props.location.state
+  const { fromSearchPage } = this.props.location.state;
 
   return (
     <body>
-        <Header></Header>
-        <div class="wrapper">
-        <section class="main-container" >
-                <div class="location" id="home">
-                    
-                    <h1 id="home">Popular on HealthyCode</h1>
-                    <SearchBar></SearchBar>
-                    <div class="box">
-                         <MovieLabel movie={fromSearchPage}></MovieLabel>   
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel> 
-                         <MovieLabel movie={fromSearchPage}></MovieLabel>   
-                    </div>
-                </div>
-            </section>
-            <FooterLinks></FooterLinks>
-            <Footer></Footer>
-        </div>
+      <Header></Header>
+      <div class="wrapper">
+        <section class="main-container">
+          <div class="location" id="home">
+            <h1 id="home">Popular on HealthyCode</h1>
+            <SearchBar></SearchBar>
+            <div class="box">
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+              <MovieLabel movie={fromSearchPage}></MovieLabel>
+            </div>
+          </div>
+        </section>
+        <FooterLinks></FooterLinks>
+        <Footer></Footer>
+      </div>
     </body>
   );
 }
--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -1,18 +1,17 @@
-
 import './SearchPage.css';
-import SearchBar from "./components/SearchBar2/SearchBar"
-import Header from "./components/Header/Header"
+import SearchBar from '../components/SearchBar2/SearchBar';
+import Header from '../components/Header/Header';
 
 function SearchPage() {
   return (
     <body>
-        <Header></Header>
-        <div class="body">
-            <form>
-                <h1> Find Everything to Watch</h1>
-                <SearchBar></SearchBar>
-            </form>
-        </div>
+      <Header></Header>
+      <div class="body">
+        <form>
+          <h1> Find Everything to Watch</h1>
+          <SearchBar></SearchBar>
+        </form>
+      </div>
     </body>
   );
 }
